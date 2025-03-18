36871e8569e9928025d8b6e46463216523514ca4
Avilash Acharya <aa.avilash@gmail.com>
2025-03-18 06:39:02 +0530

    modified the MERN app with CORS package

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@
 import express from 'express';
 import { connectDB } from './config/db.js';
 import dotenv from 'dotenv';
-
+import cors from 'cors';
 
 import ProductRoutes from './routes/product.route.js';
 
@@ -13,6 +13,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(cors());
 app.use("/api/products", ProductRoutes);
 
  
